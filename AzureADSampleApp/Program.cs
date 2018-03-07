7c6928ef4f91c66c7efe2c5b4aa810b3c510b31d
Masayuki Muto <masa.edw@gmail.com>
2018-03-07 17:05:18 +0900

    feat: Acquire token with application key

--- AzureADSampleApp/Program.cs
+++ AzureADSampleApp/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
 
 namespace AzureADSampleApp
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static string Prompt(string name)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write($"{name}: ");
+            Console.Out.Flush();
+            return Console.ReadLine();
+        }
+
+        private static async Task Main(string[] args)
+        {
+            var tenantId = Prompt("tenant id");
+            var applicationId = Prompt("application id");
+            var applicaitonKey = Prompt("application key");
+
+            var authContext = new AuthenticationContext($"https://login.microsoftonline.com/{tenantId}");
+            var token = await authContext.AcquireTokenAsync("https://management.azure.com/", new ClientCredential(applicationId, applicaitonKey));
         }
     }
 }
