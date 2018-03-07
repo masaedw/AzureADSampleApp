999d23f34cca96695cd9735282f0c1d6fb59cc11
Masayuki Muto <masa.edw@gmail.com>
2018-03-07 16:48:24 +0900

    build: Create solution

---
+++ AzureADSampleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureADSampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
